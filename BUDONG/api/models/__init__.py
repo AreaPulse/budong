269083c01a8d56f31e7c9b9be0450e0fdbf6aed4
ThuMyatHtoo29 <153301599+ThuMyatHtoo29@users.noreply.github.com>
2025-11-25 15:28:05 +0900

    Import models in __init__.py

--- BUDONG/api/models/__init__.py
+++ BUDONG/api/models/__init__.py
@@ -1,2 +1,17 @@
 # Models 모듈
+from .models import (
+    TUser,
+    TBuildingReview,
+    TUserSavedBuilding,
+    TBuilding,
+    TRealEstateTransaction,
+    TInfrastructure,
+    TSchoolDetail,
+    TParkDetail,
+    TRegion,
+    TRegionStats,
+    TEnvironmentStation,
+    TEnvironmentData,
+)
+
 
