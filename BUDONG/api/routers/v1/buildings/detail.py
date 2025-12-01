f9d19411bda2f992e0aae951c714756fb28f292b
mhibio <46654105+mhibio@users.noreply.github.com>
2025-12-01 17:35:02 +0900

    passenger_num doesnt need to multiply 80

--- BUDONG/api/routers/v1/buildings/detail.py
+++ BUDONG/api/routers/v1/buildings/detail.py
@@ -168,7 +168,7 @@ def get_building_detail(
             ext = None
             if st_complexity != None:
                 ext = {
-                    "passenger_num":st_complexity.passenger_num * 80, # 80으로 나눠진 평균값이므로 실 인원수는 80
+                    "passenger_num":st_complexity.passenger_num,
                     "complexity_rating":st_complexity.complexity_rating,
                     "line":st.line
                 }
